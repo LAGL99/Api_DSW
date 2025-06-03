90678112a723ec7b4b2061f07b9405d6edb28448
LAGL99 <luangomezlo@ittepic.edu.mx>
2025-06-03 15:31:00 -0600

    Arreglo conexion

--- utils/mongoConnection.js
+++ utils/mongoConnection.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
-await mongoose.connect(process.env.MONGODB_URL||"mongodb+srv://luangomezlo:cRbT07jnmolwp1eI@cluster0.q4oan.mongodb.net/Tienda")
+mongoose.connect(process.env.MONGODB_URL||"mongodb+srv://luangomezlo:cRbT07jnmolwp1eI@cluster0.q4oan.mongodb.net/Tienda")
 .then(()=> console.log("Mongo DB connected"))
 .catch(err => console.error("Mongo DB connecioon ERROR ", err))
+
+module.exports = mongoose;
