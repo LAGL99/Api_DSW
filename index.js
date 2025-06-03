7234ed1bdbc4fe7ad956e12716d261b0318d4218
LAGL99 <luangomezlo@ittepic.edu.mx>
2025-06-03 15:34:54 -0600

    Cambios index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const productRoutes = require('./routers/product.routers')
 const shoppingRoutes = require('./routers/shoppingcart.routers')
 const UserRoutes = require('./routers/users.routers')
-const mongoConect = require('./utils/mongoConnection')
+require('./utils/mongoConnection')
 
 const cors = require('cors');
 
@@ -14,13 +14,13 @@ app.listen(PORT,()=>{
     console.log("Server listening on PORT: ",PORT);
 });
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(cors());
 app.use("/products", productRoutes)
 app.use("/shopping", shoppingRoutes)
 app.use("/users", UserRoutes)
 
-app.get("/status", (request,response)=>{
+app.get("/", (request,response)=>{
     const status = {
         "Status":"Running"
     }
